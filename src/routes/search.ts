df348d48c10429d9e61d0fbe7b0a3673a8b53be3
FlokiTV <Afn9Wz@OKdzhe.cc>
2022-02-11 21:42:14 -0300

    update: query

--- src/routes/search.ts
+++ src/routes/search.ts
@@ -2,7 +2,8 @@ import ytsr from "ytsr";
 
 /** @type {import('@sveltejs/kit').RequestHandler} */
 export async function get({ request, url }) {
-  const searchResults = await ytsr(url.searchParams.get("q"), {
+  const q = url.searchParams.get("q");
+  const searchResults = await ytsr(q, {
     hl: "pt-BR",
     gl: "BR",
     limit: 15,
